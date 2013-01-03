9f5f269b86a88ab7f140ab103e2d148d4d4daac7
bieltv3 <bieltv.3@gmail.com>
2013-01-03 12:31:04 +0100

    first commit

---
+++ common/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 36 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#36 PREEMPT Sat Dec 22 04:11:55 BRST 2012"
+#define LINUX_COMPILE_TIME "04:11:55"
+#define LINUX_COMPILE_BY "white"
+#define LINUX_COMPILE_HOST "white-VirtualBox"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-188) "
